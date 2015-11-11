75ecbd44e7408db6aed1d36225bd1fdb0b5e9bad
NiklasJonsson6 <bknjniklas@gmail.com>
2015-11-11 13:07:35 +0100

    added snakemodel

--- src/lab1/SnakeModel.java
+++ src/lab1/SnakeModel.java
@@ -5,12 +5,34 @@ import java.awt.Dimension;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.List;
-/**
- * Created by Niklas on 10/11/15.
- */
+
 public class SnakeModel extends GameModel {
 
-    
+    public enum Directions {
+        EAST(1, 0),
+        WEST(-1, 0),
+        NORTH(0, -1),
+        SOUTH(0, 1),
+        NONE(0, 0);
+
+        private final int xDelta;
+        private final int yDelta;
+
+        Directions(final int xDelta, final int yDelta) {
+            this.xDelta = xDelta;
+            this.yDelta = yDelta;
+        }
+
+        public int getXDelta() {
+            return this.xDelta;
+        }
+
+        public int getYDelta() {
+            return this.yDelta;
+        }
+    }
+
+    private static final GameTile FOOD
 
 
     public void gameUpdate(final int lastKey) throws GameOverException {
