7e01b1a956673707105e0d2f021b1e34531ec0dc
NiklasJonsson6 <bknjniklas@gmail.com>
2015-11-11 12:43:22 +0100

    added snakemodel

---
+++ src/lab1/SnakeModel.java
@@ -0,0 +1,19 @@
+package lab1;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Created by Niklas on 10/11/15.
+ */
+public class SnakeModel extends GameModel {
+
+    
+
+
+    public void gameUpdate(final int lastKey) throws GameOverException {
+
+    }
+}
