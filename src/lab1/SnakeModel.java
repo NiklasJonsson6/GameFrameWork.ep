772a75b56b3ee5c806cccf9d083207d676971081
NiklasJonsson6 <bknjniklas@gmail.com>
2015-11-12 09:47:43 +0100

    javadoc

--- src/lab1/SnakeModel.java
+++ src/lab1/SnakeModel.java
@@ -67,7 +67,6 @@ public class SnakeModel extends GameModel {
         Snake.add(new Position(size.width/2, size.height/2));
 
         //Insert snake
-        snakePos = new Position(size.width/2, size.height/2);
         setGameboardState((Position)Snake.element(), SNAKE_TILE);
 
         //Insert food
